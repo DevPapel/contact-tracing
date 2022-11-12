be73c9a25bc03a2376da432048157081a7675795
DevPapel <cool.jef77@gmail.com>
2022-11-12 18:43:18 +0800

    Implementing Nested Dictionary

--- contact-tracing.py
+++ contact-tracing.py
@@ -1,2 +1,11 @@
 # This program allows you to save info of a person for the purpose of contact tracing
 # Program created by: Jeferson A. Tadios
+
+contact_list = {
+    "Jeferson Tadios" : {
+        "Age" : "20",
+        "Address" : "306 Cluster 3 Flexihomes",
+        "Contact#" : "01234567890"
+        }
+}
+print (contact_list)
