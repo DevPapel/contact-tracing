da0ffd081a4563570452f1c45f8008ecf14e5077
DevPapel <cool.jef77@gmail.com>
2022-11-12 19:07:02 +0800

    menu_input option 2 updated [get variables and user_input has been moved to the condition ]

--- contact-tracing.py
+++ contact-tracing.py
@@ -15,16 +15,18 @@ print("Menu:"+
 "\n3 -> Exit")
 
 menu_input = int(input("What do you want to do? "))
-user_input = input("Enter the Full name: ")
-
-get_age = contact_list[user_input]["Age"]
-get_address = contact_list[user_input]["Address"]
-get_contact = contact_list[user_input]["Contact#"]
 
 
 if menu_input == 1:
     print("Option 1")
+
+
+
 elif menu_input == 2:
+    user_input = input("Enter the Full name: ")
+    get_age = contact_list[user_input]["Age"]
+    get_address = contact_list[user_input]["Address"]
+    get_contact = contact_list[user_input]["Contact#"]
     print("Getting info of:",user_input)
     print("Age: "+str(get_age)+
     "\nAddress: "+str(get_address)+"\nContact#: "+
