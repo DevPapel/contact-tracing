0d9c6c0093543dd6a11a669861e402cf82d7c966
DevPapel <cool.jef77@gmail.com>
2022-11-12 19:29:43 +0800

    Creating a function called mainu, and inserting all conditional statement in the new function

--- contact-tracing.py
+++ contact-tracing.py
@@ -1,6 +1,36 @@
 # This program allows you to save info of a person for the purpose of contact tracing
 # Program created by: Jeferson A. Tadios
 
+def main():
+    if menu_input == 1:
+        user_input1 = input("Enter the Full name: ")
+        while user_input1 in contact_list:
+            print(user_input1,"is already in the contact list")
+            user_input1 = input("Enter the Full name: ")
+        user_input2 = input("Enter the Age: ")
+        user_input3 = input("Enter the Address: ")
+        user_input4 = input("Enter the Contact#: ")
+        contact_list[user_input1] = {
+            "Age":user_input2,
+            "Address":user_input3,
+            "Contact#":user_input4
+        }
+        print(contact_list)
+    elif menu_input == 2:
+        user_input = input("Enter the Full name: ")
+        get_age = contact_list[user_input]["Age"]
+        get_address = contact_list[user_input]["Address"]
+        get_contact = contact_list[user_input]["Contact#"]
+        print("Getting info of:",user_input)
+        print("Age: "+str(get_age)+
+        "\nAddress: "+str(get_address)+"\nContact#: "+
+        str(get_contact))
+    elif menu_input == 3:
+        print("Thank you for using this program! Have a nice day")
+        exit()
+    else:
+        print("Invalid Option")
+
 contact_list = {
     "Jeferson Tadios" : {
         "Age" : "20",
@@ -14,35 +44,8 @@ print("Menu:"+
 "\n2 -> Search"+
 "\n3 -> Exit")
 
-print(contact_list)
+
 menu_input = int(input("What do you want to do? "))
+main()
 
 
-if menu_input == 1:
-    user_input1 = input("Enter the Full name: ")
-    while user_input1 in contact_list:
-        print(user_input1,"is already in the contact list")
-        user_input1 = input("Enter the Full name: ")
-    user_input2 = input("Enter the Age: ")
-    user_input3 = input("Enter the Address: ")
-    user_input4 = input("Enter the Contact#: ")
-    contact_list[user_input1] = {
-        "Age":user_input2,
-        "Address":user_input3,
-        "Contact#":user_input4
-    }
-    print(contact_list)
-elif menu_input == 2:
-    user_input = input("Enter the Full name: ")
-    get_age = contact_list[user_input]["Age"]
-    get_address = contact_list[user_input]["Address"]
-    get_contact = contact_list[user_input]["Contact#"]
-    print("Getting info of:",user_input)
-    print("Age: "+str(get_age)+
-    "\nAddress: "+str(get_address)+"\nContact#: "+
-    str(get_contact))
-elif menu_input == 3:
-    print("Thank you for using this program! Have a nice day")
-    exit()
-else:
-    print("Invalid Option")
